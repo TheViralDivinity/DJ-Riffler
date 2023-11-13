fde765d3a86d694adf908ab3f26b1855cb3ec1bf
TheViralDivinity <landolove8@gmail.com>
2023-11-12 20:33:33 -0500

    push
    idk

--- Assets/Scripts/Code/NoteObject.cs
+++ Assets/Scripts/Code/NoteObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonController : MonoBehaviour
+public class NoteObject : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
